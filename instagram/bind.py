ad10effc6d9e5e31a5fa88497ec6b4e303c0c146
gcd0318 <gcd0318@hotmail.com>
2013-12-11 02:31:57 +0800

    correct the eror of No module named...

--- instagram/bind.py
+++ instagram/bind.py
@@ -1,7 +1,7 @@
 import urllib
-from oauth2 import OAuth2Request
+from .oauth2 import OAuth2Request
 import re
-from json_import import simplejson
+from .json_import import simplejson
 
 re_path_template = re.compile('{\w+}')
 
