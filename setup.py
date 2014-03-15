e78f98d08136fa16054bf7297635a206667907f5
Aaron Oxborrow <aaron.oxborrow@nurun.com>
2014-03-15 06:38:10 -0700

    update setup script so i can pip install

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(name="python-instagram",
-      version="0.8.0",
+setup(name="python3-instagram",
+      version="0.9.0",
       description="Instagram API client",
       license="MIT",
       install_requires=["simplejson","httplib2"],
       author="Instagram, Inc",
       author_email="apidevelopers@instagram.com",
-      url="http://github.com/Instagram/python-instagram",
+      url="http://github.com/paste/python3-instagram",
       packages = find_packages(),
       keywords= "instagram",
       zip_safe = True)
